306c9182075163773ab8efdda36ada339e22e836
ShevchenkoPavel <shevchenkopavel17@gmail.com>
2019-07-01 01:30:50 +0600

    getWeather can be customized

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ const axios = require('axios');
 const https = require('https');
 const openweatherHelper = require('openweathermap-node');
 
+const request = require("request");
+
 app.use(cors());
 app.use(bodyParser.json());
+
+const defaultApiKey = 'c20f298588437c47ed907acbd44119da';
+const defaultTown = 'Omsk';
+const defaultUnits = 'metric'; // imperial
 
 app.get('/route', function(req, res) {
   console.log('test route')
@@ -17,30 +23,6 @@ app.get('/route', function(req, res) {
 })
 
 app.get('/getWeather', function(req, res) {
-  const defaultApiKey = 'c20f298588437c47ed907acbd44119da';
-  const defaultTown = 'Omsk';
-  const defaultUnits = 'metric'; // imperial
-  // let example = 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c20f298588437c47ed907acbd44119da';
-  let example = 'https://api.openweathermap.org/data/2.5/weather?q=Omsk,ru&APPID=c20f298588437c47ed907acbd44119da';
-  // axios.get(example)
-  //   .then(result => res.send(result))
-  //   .catch(err => res.send(err))
-
-  // https.get(example, (resp) => {
-  //   let data = '';
-  //   // A chunk of data has been recieved.
-  //   console.log('resp1', resp);
-  //   resp.on('data', (chunk) => {
-  //     data += chunk;
-  //     // console.log('resp2', resp);
-  //   });
-  //   // The whole response has been received. Print out the result.
-  //   resp.on('end', () => {
-  //     console.log(JSON.parse(data).explanation);
-  //   });
-  //   }).on("error", (err) => {
-  //     console.log("Error: " + err.message);
-  //   });
 
   const helper = new openweatherHelper(
     {
@@ -59,6 +41,41 @@ app.get('/getWeather', function(req, res) {
     }
   });
 
+})
+
+app.get('/getWeatherRu', function(req, res) {
+  let cityName = req.query.city || defaultTown;
+  let units = req.query.units || defaultUnits;
+  console.log('rq', req.query);
+  console.log('unitss', units);
+
+  function sendResponse(err, data, callback){
+  	let error = null;
+  	let response = null;
+
+  	error = err;
+  	if(data){
+  		if(data.statusCode != 200){
+  			let e = JSON.parse(data.body)
+  			error = new Error(e.message);
+  			error.status = e.cod;
+  		}
+  		if(data.body){
+  			response = JSON.parse(data.body);
+  		}
+  	}
+
+  	callback(error, response);
+  }
+
+  request.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&lang=ru&units=${units}&APPID=${defaultApiKey}`,
+	 (err, data)=>{
+     function callback() {
+       console.log(data.body);
+       res.send(err || data.body);
+     }
+		sendResponse(err, data, callback);
+	})
 })
 
 app.listen(PORT, function() {
